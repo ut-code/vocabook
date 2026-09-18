3e6a5fa10f0aa83aba13dd13892806c3d575f32c
R.H <312978554+6756432598-creator@users.noreply.github.com>
2026-09-18 19:56:46 +0900

    fix: resolve merge conflicts and type errors

--- app/my-notebooks/page.tsx
+++ app/my-notebooks/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import ImportForm, {ImportTemplate,} from "@/components/my-notebooks/ImportForm";
 import { requireUser } from "@/lib/session";
-import ImportForm from "@/components/my-notebooks/ImportForm";
 import DeleteNotebookButton from "@/components/my-notebooks/DeleteNotebookButton";
 import StarColorSettings from "@/components/StarColorSettings";
 
